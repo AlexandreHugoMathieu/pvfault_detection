e3c071c04055435c4fa66968b3c888f1a370de93
AlexandreHugoMathieu <mathieu.al@laposte.net>
2022-11-04 16:54:59 +0100

    readthedocs finally working, now with less files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'pvfault_detection'
+copyright = '2022, Alexandre MATHIEU'
+author = 'Alexandre MATHIEU'
 
 release = '0.1'
 version = '0.1.0'
