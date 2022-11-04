4368707783e61f57bcbdd521aadcf409614c56e9
AlexandreHugoMathieu <mathieu.al@laposte.net>
2022-11-04 16:01:22 +0100

    Refactoring pv_models.py with docstrings

--- src/pv_models.py
+++ src/pv_models.py
@@ -6,43 +6,24 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from sklearn.neighbors import KNeighborsRegressor
 from pvlib.pvsystem import calcparams_desoto, singlediode, pvwatts_dc
-from pvlib.temperature import pvsyst_cell
 
-
-def get_temp_cell(temp_cell: pd.Series=None, t_ext: pd.Series=None, gi: pd.Series=None) -> pd.Series:
-    """ Return directly the temp_cell dataframe or roughly estimate it if not provided thanks to the standard
-    pvsyst model from pvlib
-
-
-    :param t_ext: External temperature to use for estimating the cell temperature (if temp_cell not directly provided)
-    :param temp_cell: Cell temperature used to model Imp and Vmp to calculate their variations under shading
-    :param gi: Incident (effective) irradiation [W/m2]
-
-    :return: temp_cell
-
-    References
-    ----------
-
-    : pvsyst pvlib, https://pvlib-python.readthedocs.io/en/stable/reference/generated/pvlib.temperature.pvsyst_cell.html
-    """
-    if temp_cell is None:
-        if t_ext is None:
-            raise ValueError("Either provide directly the cell temperature or the ambient temperature to estimate it")
-        else:
-            temp_cell = pvsyst_cell(gi, t_ext)
-    return temp_cell
+from src.data_check import get_temp_cell
 
 
 def inv_eff_knn(pdc_fit: pd.Series, pac_fit: pd.Series, pdc: pd.Series, n_neighbors: int = 100) -> pd.Series:
     """
-    Model and predict the AC/DC efficiency according to a KNN model
+    Fit a KNN model and predict the AC/DC efficiency
 
-    :param pdc_fit: DC power [W] to fit the model
-    :param pac_fit: AC power [W] to fit the model
-    :param pdc: DC power [W] to predict the ratio forl
-    :param n_neighbors: Number of neighbors in the KNN model
+    Parameters
+    ----------
+    pdc_fit: DC power [W] to fit the model
+    pac_fit: AC power [W] to fit the model
+    pdc: DC power [W] to predict the ratio for
+    n_neighbors: Number of neighbors in the KNN model
 
-    :return: Predicted ratios from the KNN model
+    Returns
+    -------
+    Predicted ratios from the KNN model
     """
     # Prepare the index with no nans and 0s to fit the model
     index_fit = pdc_fit.dropna().index.intersection(pac_fit.dropna().index)
@@ -62,15 +43,15 @@ def inv_eff_knn(pdc_fit: pd.Series, pac_fit: pd.Series, pdc: pd.Series, n_neighb
 
 
 def imp_teh(goa_effective: pd.Series, temp_cell: pd.Series,
-                   alpha: float, imp_ref: float,
-                   temp_stc: float = 25):
+            alpha: float, imp_ref: float,
+            temp_stc: float = 25):
     """
     Estimate Imp according to Christian Jun Qian Teh's article.
 
      References
     ----------
 
-     C. J. Q. Teh, M. Drieberg, S. Soeung and R. Ahmad, "Simple PV Modeling Under Variable Operating Conditions,"
+     .. [1] C. J. Q. Teh, M. Drieberg, S. Soeung and R. Ahmad, "Simple PV Modeling Under Variable Operating Conditions,"
      in IEEE Access, vol. 9, pp. 96546-96558, 2021, doi: 10.1109/ACCESS.2021.3094801.
     """
     imp = imp_ref * (goa_effective / 1000) * (1 + alpha * (temp_cell - temp_stc))
@@ -78,56 +59,104 @@ def imp_teh(goa_effective: pd.Series, temp_cell: pd.Series,
     return imp
 
 
-def imp_king(goa_effective: pd.Series, temp_cell: pd.Series, c1: float, alpha: float, imp_ref: float,
-             temp_stc: float = 25):
+def imp_king(goa_effective: pd.Series,
+             temp_cell: pd.Series,
+             c1: float,
+             alpha: float,
+             imp_ref: float,
+             reference_temperature: float = 25,
+             reference_irradiance: float = 1000) -> pd.Series:
     """
-    Estimate Imp according to King's model.
+
+    Parameters
+    ----------
+    goa_effective: Irradiance reaching the module's cells, after reflections and  adjustment for spectrum. [W/m2]
+    temp_cell:  Cell temperature [C].
+    c1: empirically determined coefficient
+    alpha: Maximum power current temperature coefficient at reference condition (1/C)
+    imp_ref: Power current reference [A]
+    reference_temperature:  Reference temperature at STC conditions [C]
+    reference_irradiance :  Reference Irradiance at STC conditions [W/m2]
+
+    Returns
+    -------
+    imp: pd.Series
+        Current at maximum power point
 
     References
-    ----------
+    -------
+    .. [1] King, D. et al, 2004, "Sandia Photovoltaic Array Performance
+       Model", SAND Report 3535, Sandia National Laboratories, Albuquerque,
+       NM.
 
-    Ref: King, D & Kratochvil, J & Boyson, W. (2004). Photovoltaic Array Performance Model. 8. 10.2172/919131.
     """
-    imp = imp_ref * (goa_effective / 1000 + c1 * goa_effective * goa_effective / 1000 / 1000) * (
-            1 + alpha * (temp_cell - temp_stc))
+    Ee = goa_effective / reference_irradiance
+    imp = imp_ref * (Ee + c1 * (Ee ** 2)) * (1 + alpha * (temp_cell - reference_temperature))
 
     return imp
 
 
-def vmp_king(goa_effective: pd.Series, temp_cell: pd.Series,
-             c2: float, c3: float, beta: float, vmp_ref: float,
-             temp_stc: float = 25):
+def vmp_king(goa_effective: pd.Series,
+             temp_cell: pd.Series,
+             c2: float,
+             c3: float,
+             beta: float,
+             vmp_ref: float,
+             reference_temperature: float = 25,
+             reference_irradiance: float = 1000):
     """
     Estimate Vmp at maximum power according to King's model.
+    
+    Parameters
+    ----------
+    goa_effective: Irradiance reaching the module's cells, after reflections and  adjustment for spectrum. [W/m2]
+    temp_cell:  Cell temperature [C].
+    c2: empirically determined coefficient
+    c3: empirically determined coefficient
+    beta: Open circuit voltage temperature coefficient at reference condition (V/C)
+    vmp_ref: Power voltage reference [V]
+    reference_temperature  :  Reference temperature at STC conditions [C]
+    reference_irradiance  :  Reference Irradiance at STC conditions [W/m2]
 
-    :param goa_effective:
-    :param temp_cell:
-    :param c2:
-    :param c3:
-    :param beta:
-    :param vmp_ref:
-    :param temp_stc:
-    :return:
+    Returns
+    -------
+    vmp: pd.Series
+        Voltage at maximum power point
 
     References
-    ----------
-    Ref: King, D & Kratochvil, J & Boyson, W. (2004). Photovoltaic Array Performance Model. 8. 10.2172/919131.
+    -------
+    .. [1] King, D. et al, 2004, "Sandia Photovoltaic Array Performance
+       Model", SAND Report 3535, Sandia National Laboratories, Albuquerque,
+       NM.
+
     """
+    Ee = goa_effective / reference_irradiance
+
     temp_cell_K = temp_cell + 273.15
     vmp = vmp_ref + \
-          c2 * temp_cell_K * np.log(goa_effective / 1000) + \
-          c3 * (temp_cell_K * np.log(goa_effective) / 1000) ** 2 + \
-          beta * goa_effective * (temp_cell - temp_stc)
+          c2 * temp_cell_K * np.log(Ee) + \
+          c3 * (temp_cell_K * np.log(Ee)) ** 2 + \
+          beta * goa_effective * (temp_cell - reference_temperature)
 
     return vmp
 
 
-def fit_imp_king(g_poa_effective: pd.Series, temp_cell: pd.Series, imp: pd.Series,
-                 c1: float = -1, alpha: float = 1, imp_ref: float = 100):
-    """Empirically fit Imp King model's parameters"""
+def fit_imp_king(g_poa_effective: pd.Series,
+                 temp_cell: pd.Series,
+                 imp: pd.Series):
+    """
+    Empirically fit Imp King model's parameters with brute force method relying on scipy.optimize.curve_fit
 
-    # initial guesses
-    p0_imp = (c1, alpha, imp_ref)
+    Parameters
+    ----------
+    goa_effective: Irradiance reaching the module's cells, after reflections and  adjustment for spectrum. [W/m2]
+    temp_cell:  Cell temperature [C].
+    imp: Current at the maximum power point [A]
+
+    Returns
+    -------
+    King imp's parameter inputs: c1, alpha, imp_ref
+    """
 
     # Make sure there is no Nans
     index_fit = imp.dropna().index.intersection(g_poa_effective.dropna().index).intersection(temp_cell.dropna().index)
@@ -135,22 +164,30 @@ def fit_imp_king(g_poa_effective: pd.Series, temp_cell: pd.Series, imp: pd.Serie
     g_poa_effective_fit = g_poa_effective.reindex(index_fit)
     temp_cell_fit = temp_cell.reindex(index_fit)
 
-    # Fit imp model
+    # Fit King's imp model
     def imp_to_fit(X, c1, alpha, imp_ref):
         g_poa_effective, temp_cell = X
         return imp_king(g_poa_effective, temp_cell, c1, alpha, imp_ref)
 
-    c1, alpha, imp_ref_k = curve_fit(imp_to_fit, (g_poa_effective_fit, temp_cell_fit), (imp_fit), p0_imp)[0]
+    c1, alpha, imp_ref = curve_fit(imp_to_fit, (g_poa_effective_fit, temp_cell_fit), (imp_fit))[0]
 
-    return (c1, alpha, imp_ref_k)
+    return (c1, alpha, imp_ref)
 
 
-def fit_vmp_king(g_poa_effective: pd.Series, temp_cell: pd.Series, vmp: pd.Series,
-                 c2: float = 1, c3: float = -1, beta: float = -1, vmp_ref: float = 100):
-    """Empirically fit Vmp King model's parameters"""
+def fit_vmp_king(g_poa_effective: pd.Series, temp_cell: pd.Series, vmp: pd.Series):
+    """
+    Empirically fit Vmp King model's parameters with brute force method relying on scipy.optimize.curve_fit
 
-    # initial guesses
-    p0_vmp = (c2, c3, beta, vmp_ref)
+    Parameters
+    ----------
+    goa_effective: Irradiance reaching the module's cells, after reflections and  adjustment for spectrum. [W/m2]
+    temp_cell:  Cell temperature [C].
+    imp: Voltage at the maximum power point [A]
+
+    Returns
+    -------
+    King vmp's parameter inputs: c2, c3, beta, vmp_ref
+    """
 
     # Make sure there is no Nans and no irradiation with 0s
     index_fit = vmp.dropna().index.intersection(g_poa_effective.dropna().index).intersection(temp_cell.dropna().index)
@@ -165,22 +202,33 @@ def fit_vmp_king(g_poa_effective: pd.Series, temp_cell: pd.Series, vmp: pd.Serie
         g_poa_effective, temp_cell = X
         return vmp_king(g_poa_effective, temp_cell, c2, c3, beta, vmp_ref)
 
-    c2, c3, beta, vmp_ref = curve_fit(vmp_to_fit, (g_poa_effective_fit, temp_cell_fit), (vmp_fit), p0_vmp)[0]
+    c2, c3, beta, vmp_ref = curve_fit(vmp_to_fit, (g_poa_effective_fit, temp_cell_fit), (vmp_fit))[0]
 
     return (c2, c3, beta, vmp_ref)
 
 
-def fit_pvwatt(g_poa_effective: pd.Series, pdc: pd.Series,
-               t_ext: pd.Series = None,
+def fit_pvwatt(g_poa_effective: pd.Series,
+               pdc: pd.Series,
                temp_cell: pd.Series = None,
-               pdc0: float = 40000, gamma_pdc: float = -0.002):
-    """Empirically fit fit_pvwatt model's parameters"""
+               temp_air: pd.Series = None):
+    """
+    Empirically pvwatt model's parameters with brute force method relying on scipy.optimize.curve_fit
 
-    # initial guesses P0 and gamma
-    p0 = (pdc0, gamma_pdc)  # pdc0, gamma_pdc
+    Parameters
+    ----------
+    goa_effective: Irradiance reaching the module's cells, after reflections and  adjustment for spectrum. [W/m2]
+    imp: Voltage at the maximum power point [A]
+    pdc: DC power at maximum power point [W]
+    temp_cell: Cell temperature used to model Imp and Vmp to calculate their variations under shading [C]
+    temp_air: External temperature to use for estimating the cell temperature (if temp_cell not directly provided) [C]
 
+    Returns
+    -------
+    Pvwatt Pdc model's parameter inputs: pdc0, gamma_pdc
+
+    """
     # Get temperature for PV watt model
-    temp_cell = get_temp_cell(temp_cell, t_ext, g_poa_effective)
+    temp_cell = get_temp_cell(temp_cell, temp_air, g_poa_effective)
 
     # Make sure there is no Nans and no irradiation with 0s
     index_fit = pdc.dropna().index.intersection(g_poa_effective.dropna().index).intersection(temp_cell.dropna().index)
@@ -205,34 +253,42 @@ def vi_curve_singlediode(alpha_sc: float, a_ref: float, I_L_ref: float, I_o_ref:
                          dEgdT: float = -0.0002677) -> pd.DataFrame:
     """
     Draw the IV curve according to DeSoto method and the single Diode model.
-    # Add ref from pvlib
 
-     :param alpha_sc: The short-circuit current temperature coefficient of the  module in units of A/C.
-     :param a_ref: The product of the usual diode ideality factor (n, unitless),
+    Parameters
+    ----------
+    alpha_sc: The short-circuit current temperature coefficient of the  module in units of A/C.
+    a_ref: The product of the usual diode ideality factor (n, unitless),
          number of cells in series (Ns), and cell thermal voltage at reference
          conditions, in units of V.
-     :param IL: The light-generated current (or photocurrent) at reference conditions, in amperes.
-     :param I_o_ref: The dark or diode reverse saturation current at reference conditions, in amperes.
-     :param Rs: The series resistance at reference conditions, in ohms.
-     :param Rsh: The shunt resistance at reference conditions, in ohms.
-     :param n_points: Number of points in the desired IV curve
-     :param effective_irradiance: The irradiance (W/m2) that is converted to photocurrent.
-     :param temp_cell:  The average cell temperature of cells within a module in C.
-     :param EgRef: The energy bandgap at reference temperature in units of eV.
+    IL: The light-generated current (or photocurrent) at reference conditions, in amperes.
+    I_o_ref: The dark or diode reverse saturation current at reference conditions, in amperes.
+    Rs: The series resistance at reference conditions, in ohms.
+    Rsh: The shunt resistance at reference conditions, in ohms.
+    n_points: Number of points in the desired IV curve
+    effective_irradiance: The irradiance (W/m2) that is converted to photocurrent.
+    temp_cell:  The average cell temperature of cells within a module in C.
+    EgRef: The energy bandgap at reference temperature in units of eV.
          1.121 eV for crystalline silicon. EgRef must be >0.  For parameters
          from the SAM CEC module database, EgRef=1.121 is implicit for all
          cell types in the parameter estimation algorithm used by NREL.
-     :param dEgdT:  The temperature dependence of the energy bandgap at reference
+    dEgdT:  The temperature dependence of the energy bandgap at reference
          conditions in units of 1/K. May be either a scalar value
          (e.g. -0.0002677 as in [1]_) or a DataFrame (this may be useful if
          dEgdT is a modeled as a function of temperature). For parameters from
          the SAM CEC module database, dEgdT=-0.0002677 is implicit for all cell
          types in the parameter estimation algorithm used by NREL.
 
-     :return: IV curve
+    Returns
+    -------
+    IV curve in a pd.DataFrame:
          * i - IV curve current in amperes.
          * v - IV curve voltage in volts.
-     """
+
+    References
+    -------
+    Strongly inspired/taken from: https://pvlib-python.readthedocs.io/en/v0.9.0/auto_examples/plot_singlediode.html
+
+    """
     # adjust the reference parameters according to the operating conditions (effective_irradiance, temp_cell)
     # using the De Soto model:
     IL, I0, Rs, Rsh, nNsVth = calcparams_desoto(effective_irradiance, temp_cell,
@@ -255,23 +311,48 @@ def vi_curve_singlediode(alpha_sc: float, a_ref: float, I_L_ref: float, I_o_ref:
     return curve_df
 
 
-def get_Pmpp(curve_tmp, VI_max=False, v_col="v", i_col="i"):
-    p = curve_tmp[v_col] * curve_tmp[i_col]
+def get_Pmpp(iv_curve: pd.DataFrame, VI_max=False, v_col="v", i_col="i"):
+    """
+    Return the maximum power-point from an IV curve
+
+    Parameters
+    ----------
+    iv_curve: Dataframe with the "i" and "v" points
+    VI_max: also return "Vmpp" and "Impp" at the maximum power point
+    v_col: Voltage column
+    i_col: Current column
+    """
+    p = iv_curve[v_col] * iv_curve[i_col]
     Pmpp = p.max()
 
     if VI_max:
-        I_mpp, V_mpp = curve_tmp.loc[p.idxmax(), [i_col, v_col]].values
+        I_mpp, V_mpp = iv_curve.loc[p.idxmax(), [i_col, v_col]].values
         return Pmpp, I_mpp, V_mpp
     else:
         return Pmpp
 
 
-def curve_plot(curve, legend: str = None, show_Pmax=True):
-    plt.plot(curve["v"], curve["i"], label=legend)
+def curve_plot(iv_curve, v_col="v", i_col="i", legend: str = None, show_Pmax=True):
+    """ Plot IV curve with maximum power point"""
+    ax = plt.plot(iv_curve[v_col], iv_curve[i_col], label=legend)
     if show_Pmax:
-        Pmax, I_max, V_max = get_Pmpp(curve, VI_max=True)
+        Pmax, I_max, V_max = get_Pmpp(iv_curve, VI_max=True)
         plt.plot(V_max, I_max, label=(legend + "-Pmpp" if legend is not None else "Pmpp"), color="red", marker="o")
     plt.title("VI Curve")
     plt.ylabel("Intensity [A]")
     plt.xlabel("Voltage [V]")
     plt.legend()
+
+    return ax
+
+
+if __name__ == '__main__':
+    import pvlib
+    from src.utils.helio_fmt import setup_helio_plt
+
+    pv_params = pvlib.pvsystem.retrieve_sam('cecmod')['Hanwha_Q_CELLS_Q_PEAK_DUO_G5_SC_325']
+    vi_curve = vi_curve_singlediode(pv_params["alpha_sc"], pv_params["a_ref"], pv_params["I_L_ref"],
+                                    pv_params["I_o_ref"], pv_params["R_sh_ref"], pv_params["R_s"])
+
+    setup_helio_plt()
+    curve_plot(vi_curve)
